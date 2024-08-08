34b54ef97a8b4a47d9decb477ae08ec6d3a37e89
Shaylene Sunday <allkatt@yahoo.com>
2024-08-08 14:21:05 -0600

    added index.html, css and js basic codes

---
+++ app.js
@@ -0,0 +1,5 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const resetButton = document.getElementsByClassName('btn_reset');
+
+let missed = 0;
