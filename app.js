d63b983cc4449bebf3d7c6df212308013d0897b3
Shaylene Sunday <allkatt@yahoo.com>
2024-08-10 14:05:39 -0600

    added js

--- app.js
+++ app.js
@@ -3,3 +3,34 @@ const phrase = document.getElementById('phrase');
 const resetButton = document.getElementsByClassName('btn_reset');
 
 let missed = 0;
+
+// return a random phrase from an array
+const getRandomPhraseArray = arr => {
+
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener ( 'click', (e) => {
+    resetButton.style.display = 'hide';
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener ( 'click', (e) => {
+
+});
