5a04c80094b44cd9dad003b69d489722707ba1cc
Shaylene Sunday <allkatt@yahoo.com>
2024-08-12 13:28:05 -0600

    trying to get the phrases uploaded

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const resetButton = document.getElementsByClassName('btn_reset');
+const startButton = document.getElementsByClassName('start');
+const overlay = document.querySelector('overlay');
 
 let missed = 0;
+
+const phrases = [
+
+    'After Tuesday even the calendar says WTF',
+    'Slow Children at play',
+    'Get out of my kitchen',
+    'But Mommy said',
+    'That is what makes you beautiful',
+    'Beware of dog'
+];
+
+resetButton.addEventListener('click', (e) => {
+    startGame();
+});
+
+function startGame() {
+   overlay.style.display = "none";
+   addPhraseToDisplay(getRandomPhraseArray(phrases));
+
+};
+
+// 
+function stub () {
+let randomNumber = getRandomPhraseArray.length;
+randomNumber[3];
+};
 
 // return a random phrase from an array
 const getRandomPhraseArray = arr => {
 
-}
+};
 
 // adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
@@ -27,7 +55,7 @@ const checkWin = () => {
 
 // listen for the start game button to be pressed
 startButton.addEventListener ( 'click', (e) => {
-    resetButton.style.display = 'hide';
+    overlay.style.display = 'none';
 });
 
 // listen for the onscreen keyboard to be clicked
