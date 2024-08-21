c1b098ce0f82fc3c149a142b4699a3a04db8d217
Shaylene Sunday <allkatt@yahoo.com>
2024-08-21 13:11:37 -0600

    updated the random phrases

--- app.js
+++ app.js
@@ -9,22 +9,30 @@ let missed = 0;
 
 // phrases array
 
-const phrases = [
-    'better late than never',
-    'Slow Children at play',
-    'Get out of my kitchen',
-    'But Mommy said',
-    'That is what makes you beautiful',
-    'Beware of dog'
-];
 
 
 // arrow functions
 
 // return a random phrase from an array
 const getRandomPhraseArray = arr => {
-  
-        
+    Math.floor( Math.random() * arr.length );
+    const phrases = [
+        'better late than never',
+        'Slow Children at play',
+        'Get out of my kitchen',
+        'But Mommy said',
+        'That is what makes you beautiful',
+        'Beware of dog'
+    ];
+// output of words
+    const words = phrases.split(' ');
+
+// output of characters
+    const characters = phrases.split('');
+
+// output the entire phrases
+    const phrasesCopy = phrases.split();
+
 };
 
 // adds the letters of a string to the display
@@ -43,10 +51,10 @@ const checkWin = () => {
 
 }
 
-// listen for the start game button to be pressed
-resetButton.addEventListener ( 'click', (e) => {
+// listen for the start game button to be pressed (maybe startButton instead of resetButton)
+// resetButton.addEventListener ( 'click', (e) => {
 
-});
+// });
 
 // listen for the onscreen keyboard to be clicked
 qwerty.addEventListener ( 'click', (e) => {
@@ -54,4 +62,4 @@ qwerty.addEventListener ( 'click', (e) => {
 });
 
 // add event listener to reset button
-resetButton.addEventListener('click');
+// resetButton.addEventListener('click');
